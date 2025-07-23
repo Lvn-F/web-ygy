1da1761af4bc772beb12ff785fbfe45f21f97746
Lvn-F <lovanfirdaus@gmail.com>
2025-07-23 09:28:31 +0700

    Addding files

---
+++ js/script.js
@@ -0,0 +1,67 @@
+/* Topic Slider */
+let CurrentSlide = 0;
+const innovationSlide = document.querySelectorAll('.slideInnovation');
+const thumbnails = document.querySelectorAll('.Thumbnail');
+const totalSlides = innovationSlide.length;
+
+function updateTopicSlider() {
+    innovationSlide.forEach(slideInnovation => slideInnovation.classList.remove('Active'));
+    thumbnails.forEach(Thumbnail => Thumbnail.classList.remove('Active'));
+
+    innovationSlide[CurrentSlide].classList.add('Active');
+    thumbnails[CurrentSlide].classList.add('Active');
+}
+
+function changeTopicSlide(direction) {
+    CurrentSlide += direction;
+    if (CurrentSlide < 0) {
+        CurrentSlide = totalSlides - 1;
+    } else if (CurrentSlide >= totalSlides) {
+        CurrentSlide = 0;
+    }
+    updateTopicSlider();
+}
+function goToSlide(slideIndex) {
+    CurrentSlide = slideIndex;
+    updateTopicSlider();
+}
+
+thumbnails.forEach((Thumbnail, index) => {
+    Thumbnail.addEventListener('click', function(e) {
+        e.preventDefault();
+        goToSlide(index);
+    });
+});
+
+
+const nav = document.querySelector('.navbutton');
+const navback = document.querySelector('.navback');
+const navCards = document.querySelectorAll('.navcard');
+nav.addEventListener('click', () => {
+    navCards.forEach(navcard => {
+        navcard.classList.add('show');
+        navcard.classList.remove('hidden');
+    });
+});
+navback.addEventListener('click', () => {
+    navCards.forEach(navcard => {
+        navcard.classList.remove('show');
+        navcard.classList.add('hidden');
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        const navHeight = nav.offsetHeight;
+
+        window.scrollTo({
+            top: targetSection.offsetTop - navHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
